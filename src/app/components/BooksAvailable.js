2ace2cb00eac859c570dd3af1bcde23eec995864
Matias Seitour <matias.seitour01@gmail.com>
2023-08-02 22:33:39 -0300

    delete component

--- src/app/components/BooksAvailable.js
+++
@@ -1,8 +0,0 @@
-"use client"
-
-export const BooksAvailable = ({ booksLength }) => {
-
-    return (
-        <h2 className="text-center text-white text-4xl mb-10" > Libros Disponibles: {booksLength}</h2>
-    )
-}
