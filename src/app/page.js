28ded160982420179ae336cd27896cd787466c68
Matias Seitour <matias.seitour01@gmail.com>
2023-08-02 22:33:33 -0300

    feat(page) add font

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import { BooksContainer } from "./components/BooksContainer";
+import { paytone_One } from "../../utils/fonts";
 
 export default function Home() {
   return (
     <main className="min-h-screen w-full pt-20 px-4">
-      <h1 className="text-white text-6xl text-center mb-4">Books Store</h1>
+      <h1 className={`text-white text-6xl text-center mb-4 ${paytone_One.className}`}>Books Store</h1>
       <BooksContainer />
     </main>
   )
