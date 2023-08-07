cd24658f1ff77cd8527e92f9641463362fbb4955
Matias Seitour <matias.seitour01@gmail.com>
2023-08-06 22:57:24 -0300

    feat(book) add loading skeleton and some styles

--- src/app/components/Book.js
+++ src/app/components/Book.js
@@ -1,27 +1,32 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { gsap } from "gsap-trial"
 import { ScrollTrigger } from "gsap-trial/all"
 import Image from "next/image"
 
 
-export function Book({ addMyListBook, isbnArr, book }) {
+export function Book({ addMyListBook, isbnArr, book, genreSelected, myListBookState, searchBook }) {
+
+    const [loadBookState, setLoadBookState] = useState(false);
+
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger)
+
+        setLoadBookState(true);
+        setTimeout(() => {
+            setLoadBookState(false);
+        }, 500)
 
         const tl2 = gsap.timeline({
             scrollTrigger: {
                 trigger: `.book-${book.ISBN}`,
-                markers: true,
-                start: "top center",
-                end: "start 70%",
+                // markers: true,
+                start: "start 90%",
+                end: "start 90%",
             }
         })
 
         const booksElements = gsap.utils.toArray(`.book-info__${book.ISBN} p`)
-        console.log(booksElements)
 
-
-        // booksElements.forEach((element) => {
         tl2.fromTo(`.book-${book.ISBN}`, {
             opacity: 0,
             duration: .5,
@@ -30,15 +35,6 @@ export function Book({ addMyListBook, isbnArr, book }) {
                 opacity: 1,
                 duration: .5,
             })
-
-        // tl2.fromTo(`.book-${book.ISBN} p`, {
-        //     opacity: 0,
-        //     duration: .5,
-        // },
-        //     {
-        //         opacity: 1,
-        //         duration: .5,
-        //     })
 
         booksElements.forEach((element) => {
             tl2.fromTo(element, {
@@ -50,36 +46,66 @@ export function Book({ addMyListBook, isbnArr, book }) {
                     opacity: 1,
                     yPercent: -0,
                     duration: .2,
-                })
+                }, "-=.18")
         })
-    }, [])
+    }, [genreSelected, myListBookState, searchBook])
 
     return (
-        <li
-            onClick={() => addMyListBook(book)}
-            className={`rounded-md h-auto book max-h-[18rem] w-full max-w-5xl flex flex-row justify-between relative book-${book.ISBN}`} key={book.title}>
-            {isbnArr.includes(book.ISBN) && <span className="absolute top-2 right-2 bg-black p-2 rounded-md text-sm">Agregado a mi lista</span>}
-            <Image width={1000} height={1000} className="w-56 min-w-[10rem] h-full object-cover" alt={book.title} src={book.cover} />
-            <div className={`p-2 flex flex-col gap-2 h-full w-full book-info__${book.ISBN}`}>
-                <p className={`text-white text-xl font-semibold book-title`}>{book.title}</p>
-                <p className="text-white/70 book-synopsis">{book.synopsis}</p>
-                {/* <div className="flex flex-col"> */}
-                <p className="book-genre">{book.genre}</p>
-                <p className="book-date">Fecha de publicación: {book.year}</p>
-                {/* </div> */}
-                {/* <div className="w-auto"> */}
-                {/* <div className="flex "> */}
-                <p className="book-author">Autor/a: {book.author.name}</p>
-                {/* </div> */}
-                <ul className="">
-                    <li>Otros libros</li>
-                    {book.author.otherBooks.map((otherBook) => (
-                        <li className="text-sm">{otherBook}</li>
-                    ))}
-                </ul>
-                <p>Número de páginas: {book.pages}</p>
-                {/* </div> */}
-            </div>
-        </li>
+        <>
+            {loadBookState ?
+                <>
+                    <li
+                        className={`rounded-md h-auto book w-full max-w-5xl flex flex-row justify-between relative book-${book.ISBN} border border-white/30`}>
+                        <div className="w-72 bg-white/10"></div>
+                        <div className={`p-2 flex flex-col gap-4 h-full w-full book-info__${book.ISBN}`}>
+                            <div className="flex-col gap-4 flex">
+                                <div className={`bg-white/10 w-40 h-10`}></div>
+                                <div className="bg-white/10 w-64 h-10"></div>
+                                <div className={`bg-white/10 w-40 h-10`}></div>
+                                <div className="bg-white/10 w-64 h-10"></div>
+                                <div className={`bg-white/10 w-40 h-10`}></div>
+                                <div className="bg-white/10 w-64 h-10"></div>
+                            </div>
+                        </div>
+                    </li>
+                </>
+                :
+                <li
+                    className={`rounded-md h-auto book w-full max-w-5xl flex flex-row justify-between relative book-${book.ISBN} border border-white/30`}>
+                    <button onClick={() => addMyListBook(book)} className={isbnArr.includes(book.ISBN) ? "cursor-pointer absolute top-2 right-2 bg-white text-black p-2 rounded-md text-sm scale-100 transition-all font-medium" : "scale-100 cursor-pointer transition-all absolute top-2 right-2 bg-white text-black p-2 rounded-md text-sm font-medium"} >{isbnArr.includes(book.ISBN) ? "✔️ Agregado a mi lista" : "Agregar a mi lista"}</button>
+                    <div className="w-72">
+                        <Image width={1000} height={1000} className="rounded-l-md w-full h-full object-cover" alt={book.title} src={book.cover} />
+                    </div>
+                    <div className={`p-2 flex flex-col gap-4 h-full w-full book-info__${book.ISBN}`}>
+                        <div className="flex-col gap-4 flex">
+                            <p className={`text-white text-xl font-semibold book-title`}>{book.title}</p>
+                            <p className="text-white/70 book-synopsis max-w-sm">{book.synopsis}</p>
+                            <p className="book-genre text-white/70"><span className="text-white">Género:</span> {book.genre}</p>
+                            <p className="book-date text-white/70"><span className="text-white">Fecha de publicación:</span> {book.year}</p>
+
+                        </div>
+                        <div className="flex flex-col text-left gap-2">
+                            <p className="book-author text-white/70"><span className="text-white">Autor/a:</span> {book.author.name}</p>
+                            {
+                                book.author.otherBooks.length != 0 &&
+                                <>
+                                    <p>Otros libros</p>
+                                    <ul className="">
+                                        {book.author.otherBooks.map((otherBook) => (
+                                            <li key={otherBook} className="text-sm text-white/70">{otherBook}</li>
+                                        ))}
+                                    </ul>
+
+                                </>
+                            }
+                            <p className="text-white/70"><span className="text-white">Número de páginas:</span> {book.pages}</p>
+
+                        </div>
+                    </div>
+                </li>
+
+            }
+        </>
+
     )
 }
