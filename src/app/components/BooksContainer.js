461e418ad013f77ca434ed30f9a38ee9b5a15d44
Matias Seitour <matias.seitour01@gmail.com>
2023-08-06 22:56:52 -0300

    feat(booksContainer) add new styles, pages filter, nav options and remove some states

--- src/app/components/BooksContainer.js
+++ src/app/components/BooksContainer.js
@@ -1,22 +1,15 @@
 "use client"
 
-import Image from "next/image";
 import booksData from "../../../utils/books.json"
-import { useEffect, useMemo, useRef, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { paytone_One } from "../../../utils/fonts";
 import { gsap } from "gsap-trial";
 import SplitType from "split-type";
-import { ScrollTrigger } from "gsap-trial/all";
-import useIntersection from "../hooks/useIntersection";
 import { Book } from "./Book";
 
 export default function BooksContainer() {
     const { library } = booksData;
     const booksLibrary = library.map((bookItem) => bookItem.book);
-
-    const app = useRef(null)
-    // const books = useRef(null)
-    // const bookObserver = useIntersection(books);
 
     const [isbnArr, setIsbnArr] = useState([]);
     const [myListBook, setMyListBook] = useState([]);
@@ -24,8 +17,7 @@ export default function BooksContainer() {
     const [genreMyListBookSelected, setGenreMyListBookSelected] = useState("Todos");
     const [myListBookState, setMyListBookState] = useState(false);
     const [searchBook, setSearchBook] = useState("");
-    const [searchMyListBook, setSearchMyListBook] = useState("");
-    const [bookDetails, setBookDetails] = useState(null)
+    const [pagesFilter, setPagesFilter] = useState(1400)
 
 
     function handleLocalStorage() {
@@ -49,32 +41,22 @@ export default function BooksContainer() {
     }
 
     const handleSearch = (e) => {
-        if (!myListBookState) {
-            setSearchBook(e.target.value);
-        }
-        else {
-            setSearchMyListBook(e.target.value)
-        }
+        setSearchBook(e.target.value);
+    };
+    const handlePageFilter = (e) => {
+        setPagesFilter(e.target.value);
     };
 
     function filterByGenre(books) {
-        if (!myListBookState) {
-            return books.filter((item) => {
-                if (item.title.toString().toLowerCase().includes(searchBook.toLowerCase())) {
-                    return item;
-                }
-            });
-        }
-        else {
-            return books.filter((item) => {
-                if (item.title.toString().toLowerCase().includes(searchMyListBook.toLowerCase())) {
-                    return item;
-                }
-            });
-        }
+        return books.filter((item) => {
+            if (item.title.toString().toLowerCase().includes(searchBook.toLowerCase()) && item.pages <= pagesFilter) {
+                return item;
+            }
+        });
     }
 
     const booksFilter = useMemo(() => {
+        window.scrollTo(0, 0)
         if (genreSelected === "Todos") {
             setGenreSelected("Todos")
             return filterByGenre(booksLibrary);
@@ -82,26 +64,23 @@ export default function BooksContainer() {
         }
         else {
             const genreFilter = booksLibrary.filter((book) => {
-                if (book.genre != genreSelected) return false;
+                if (book.genre != genreSelected && book.pages > pagesFilter) return false;
                 setGenreSelected(book.genre)
                 return true;
             })
             return filterByGenre(genreFilter)
         }
 
-    }, [genreSelected, searchBook])
+    }, [genreSelected, searchBook, pagesFilter])
 
     const myListBookFilter = useMemo(() => {
         if (genreMyListBookSelected === "Todos") {
-            console.log(genreMyListBookSelected)
             setGenreMyListBookSelected("Todos")
             return filterByGenre(myListBook);
         }
         else {
             const genreFilter = myListBook.filter((book) => {
-                if (book.genre != genreMyListBookSelected) {
-                    return false;
-                }
+                if (book.genre != genreMyListBookSelected) return false;
                 setGenreMyListBookSelected(book.genre)
                 return true;
             })
@@ -109,8 +88,7 @@ export default function BooksContainer() {
 
         }
 
-    }, [genreMyListBookSelected, searchMyListBook, myListBookState, myListBook])
-
+    }, [genreMyListBookSelected, myListBookState, myListBook, searchBook])
 
 
     const genreList = booksLibrary.reduce((acc, item) => {
@@ -151,8 +129,6 @@ export default function BooksContainer() {
         return () => listenLocalStorage();
     }, [])
 
-    const genreSelectedCount = genreSelected != "Todos" ? getNumberGenre.find(item => item.genre == genreSelected) : null;
-
     useEffect(() => {
         let title = new SplitType(".title");
         let chars = title;
@@ -170,61 +146,35 @@ export default function BooksContainer() {
         tl.fromTo("h1", { ease: "power3.inOut", yPercent: 150, duration: 1, scale: 2 }, { ease: "power3.inOut", yPercent: 0, duration: 1, scale: 1 }, "-=.5")
         tl.fromTo(".books-options", { opacity: 0, duration: .5 }, { opacity: 1, duration: .5 }, "-=.4")
 
-        // booksElements.forEach((element) => {
-        //     gsap.fromTo(element, {
-        //         scrollTrigger: {
-        //             trigger: element,
-        //             markers: true,
-        //             start: "start 70%",
-        //             end: "start 70%",
-
-
-        //         },
-        //         opacity: 0,
-        //         duration: .5,
-        //     },
-        //         {
-        //             scrollTrigger: {
-        //                 trigger: element,
-        //                 markers: true,
-        //                 start: "start 70%",
-        //                 end: "start 70%",
-        //             },
-        //             opacity: 1,
-        //             duration: .5,
-        //         })
-        // })
-        // tl.fromTo("li", { opacity: 0, duration: .5, stagger: 0.05 }, { opacity: 1, duration: .5, stagger: 0.05 }, "-=.4")
     }, [])
 
-
-
     return (
-        <div ref={app} className={`h-auto p-4 w-full overflow-hidden`}>
-            <h1 className={`title text-white text-6xl text-center mb-4 ${paytone_One.className} z-20`}>Books Store</h1>
+        <div className={`h-auto p-4 w-full max-w-7xl mx-auto`}>
+            <div className="flex justify-center">
+                <h1 className={`title text-white text-6xl text-center mb-4 ${paytone_One.className} max-w-sm`}>Books Store</h1>
+            </div>
 
-            <section className="">
-                <div className="books-options">
-                    <div className="h-auto p-2 w-full flex justify-center items-center">
-                        <h2 className="w-full max-w-xl text-center text-2xl text-white/70">Descubre tesoros literarios en nuestra librería: una ventana a la imaginación y el conocimiento, donde cada página te invita a soñar y aprender.</h2>
-                    </div>
-                    <div className="sticky top-0 left-0 bg-red-300 h-auto">
-                        <h3 className={`text-left text-white text-4xl mb-10 ${paytone_One.className}`} > Books Availables: {booksLibrary.length - myListBook.length}</h3>
-                        {genreSelected != "Todos" &&
-                            <h3 className={`text-left text-white text-4xl mb-10 ${paytone_One.className}`} > {genreSelected}: {genreSelectedCount.count}</h3>
-                        }
-                        <div className="flex justify-between mb-4">
-                            <select className="text-black p-2 h-10 rounded-md font-bold outline-none shadow-lg shadow-white/30 hover:bg-[#ddd] transition-all" value={genreSelected} onChange={(e) => setGenreSelected(e.target.value)}>
-                                <option key={1231} value="Todos">{`Todos (${booksLibrary.length})`}</option>
-                                {getNumberGenre.map((genre) => (
-                                    <option key={genre.genre} className="text-black" value={genre.genre}>{`${genre.genre} (${genre.count})`}</option>
-                                ))}
-                            </select>
-                            <button onClick={() => setMyListBookState(true)} className="shadow-lg shadow-white/30 text-center mb-4 p-2 font-bold rounded-md bg-white text-black hover:bg-white/90 transition-all">
-                                My books: {myListBook.length}
-                            </button>
+            <section className="books-options">
+                <div className="h-auto p-2 w-full flex justify-center items-center mb-10">
+                    <h2 className="w-full max-w-xl text-center text-2xl text-white/70">Descubre tesoros literarios en nuestra librería: una ventana a la imaginación y el conocimiento, donde cada página te invita a soñar y aprender.</h2>
+                </div>
+                <h3 className={` text-left text-white text-4xl mb-10 ${paytone_One.className}`} > Books Availables: {booksLibrary.length - myListBook.length}</h3>
+                <nav className="sticky top-4 z-10 mb-10 bg-[#222a] backdrop-blur-sm p-2 flex items-center justify-between rounded-md">
+                    <div className="flex justify-between gap-4 w-full">
+                        <select className="text-black p-2 h-10 rounded-md font-bold outline-none  hover:bg-[#ddd] transition-all" value={genreSelected} onChange={(e) => {
+                            setGenreMyListBookSelected(e.target.value);
+                            setGenreSelected(e.target.value);
+                        }}>
+                            <option key={1231} value="Todos">{`Todos (${booksLibrary.length})`}</option>
+                            {getNumberGenre.map((genre) => (
+                                <option key={genre.genre} className="text-black" value={genre.genre}>{`${genre.genre} (${genre.count})`}</option>
+                            ))}
+                        </select>
+                        <div>
+                            <input onChange={handlePageFilter} className="text-white bg-white" type="range" min={0} max={1400} value={pagesFilter} />
+                            <output>{pagesFilter}</output>
                         </div>
-                        <div className="flex w-72 items-center justify-center rounded-md border border-white/20 mb-4">
+                        <div className="flex w-96 items-center h-10 justify-center rounded-md border border-white/20 focus-within:border-white">
                             <input
                                 onChange={handleSearch}
                                 value={searchBook}
@@ -232,54 +182,23 @@ export default function BooksContainer() {
                                 className="h-full w-full rounded-r-md p-1 text-left font-bold text-white outline-none bg-transparent"
                             />
                         </div>
+                        <button onClick={() => setMyListBookState(prev => !prev)} className="h-10 text-center p-2 font-bold rounded-md bg-white text-black hover:bg-white/90 transition-all">
+                            My books: {myListBook.length}
+                        </button>
                     </div>
-
-                </div>
-                <ul className="flex flex-col gap-4 h-auto items-center justify-center relative">
-                    {booksFilter.map((book) => (
-                        <Book isbnArr={isbnArr} book={book} addMyListBook={addMyListBook} />
-                    ))}
-                </ul>
-            </section>
-            <article>
-                <div className={myListBookState ? "fixed top-0 right-0 w-full sm:w-[600px] h-full bg-[#222] transition-all overflow-y-scroll px-4" : "fixed top-0 -right-[100%] w-full sm:w-[600px] h-full bg-[#222] transition-all px-4"}>
-                    <span onClick={() => setMyListBookState(false)} className="text-3xl inline-block translate-x-2 cursor-pointer">X</span>
-                    <h3 className={`text-left text-white text-4xl mb-10 ${paytone_One.className} text-center`} > My books list</h3>
-                    <div className="flex gap-4 justify-between items-center">
-                        <div className="flex w-72 h-full items-center justify-center rounded-md border border-white/20">
-                            <input
-                                onChange={handleSearch}
-                                value={searchMyListBook}
-                                placeholder="¿Qué libro estás buscando?"
-                                className="h-full w-full rounded-r-md p-2 text-left font-bold text-white outline-none bg-transparent"
-                            />
-                        </div>
-                        <select className="text-black p-2 h-10 rounded-md font-bold outline-none shadow-lg shadow-white/30 hover:bg-[#ddd] transition-all" value={genreMyListBookSelected} onChange={(e) => setGenreMyListBookSelected(e.target.value)}>
-                            <option key={1231} value="Todos">{`Todos (${myListBookFilter.length})`}</option>
-                            {getNumberGenre.map((genre) => (
-                                <option key={genre.genre} className="text-black" value={genre.genre}>{`${genre.genre} (${genre.count})`}</option>
-                            ))}
-                        </select>
-                    </div>
-                    {myListBook.length == 0 ?
-                        <p className={`text-center text-2xl ${paytone_One.className}`}>No hay libros en tu lista</p>
-
+                </nav>
+                <ul className="flex flex-col gap-8 h-auto items-center justify-center relative">
+                    {!myListBookState ? booksFilter.map((book) => (
+                        <Book isbnArr={isbnArr} book={book} addMyListBook={addMyListBook} key={book.ISBN} genreSelected={genreSelected} myListBookState={myListBookState} searchBook={searchBook} />
+                    ))
                         :
-                        <ul className="grid sm:grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-4 h-auto p-4">
-                            {myListBookFilter.map((book) => (
-                                <li onClick={() => addMyListBook(book)} className={`rounded-md h-full w-full flex flex-col gap-2 relative bg-black`} key={book.title}>
-                                    {isbnArr.includes(book.ISBN) && <span className="absolute top-2 right-2 bg-black p-2 rounded-md text-sm">Quitar de mi lista</span>}
-                                    <Image width={1000} height={1000} className="rounded-t-md w-full h-full object-cover" alt={book.title} src={book.cover} />
-                                    <div className="p-2 flex flex-col gap-2 h-80">
-                                        <p className={`text-white text-xl font-semibold`}>{book.title}</p>
-                                        <p className="text-white/70">{book.synopsis}</p>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
+
+                        myListBookFilter.map((book) => (
+                            <Book isbnArr={isbnArr} book={book} addMyListBook={addMyListBook} key={book.ISBN} genreSelected={genreSelected} myListBookState={myListBookState} searchBook={searchBook} />
+                        ))
                     }
-                </div>
-            </article>
-        </div>
+                </ul>
+            </section >
+        </div >
     )
 }
